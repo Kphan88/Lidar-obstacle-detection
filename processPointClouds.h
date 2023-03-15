8d7e4ceb10884f33f903d600cecd3e942078cf24
Kevin Phan <nguyentdhbk@gmail.com>
2023-03-15 02:26:09 +0000

    Add files via upload

--- processPointClouds.h
+++ processPointClouds.h
@@ -18,6 +18,87 @@
 #include <ctime>
 #include <chrono>
 #include "render/box.h"
+//
+
+
+// Structure to represent node of kd tree
+struct Node
+{
+	std::vector<float> point;
+	int id;
+	Node* left;
+	Node* right;
+
+	Node(std::vector<float> arr, int setId)
+	:	point(arr), id(setId), left(NULL), right(NULL)
+	{}
+};
+
+struct KdTree
+{
+	Node* root;
+
+	KdTree()
+	: root(NULL)
+	{}
+
+	// Define helper fnc to insert
+	void insertHelper(Node** node, uint depth, std::vector<float> point, int id )
+	{
+		if( *node ==NULL)
+			*node =  new Node(point, id);
+		else
+		{
+			uint dims = depth %2; 
+
+			if(point[dims] < (*node)->point[dims])
+				insertHelper(&((*node)->left), depth +1, point, id);
+			else
+				insertHelper(&((*node)->right), depth +1, point, id);
+
+		}
+						
+	}
+	// insert new node:
+	void insert(std::vector<float> point, int id)
+	{ 
+		insertHelper(&root, 0, point, id);
+	}
+
+	// helper fnc for seach:
+
+	void searchHelper(Node* node, std::vector<float>target, float distanceTol, uint depth,std::vector<int>& ids)
+	{
+		if (node != NULL)
+		{ 
+			//if ((abs(node->point[0]-target[0])<= distanceTol) && (abs(node->point[1]-target[1])<=distanceTol))
+			if ( (node->point[0]>=(target[0]-distanceTol)&& node->point[0]<=(target[0]+distanceTol))&&(node->point[1]>=(target[1]-distanceTol)&& node->point[1]<=(target[1]+distanceTol)))
+			{
+				//float distance = sqrt(pow(node->point[0]-target[0],2) + pow((node->point[1]-target[1]),2));
+				float distance = sqrt((node->point[0]-target[0])*(node->point[0]-target[0]) + (node->point[1]-target[1])*(node->point[1]-target[1]));
+				if (distance <= distanceTol)
+					ids.push_back(node->id);
+
+			}
+			if((target[depth%2]-distanceTol)<node->point[depth%2])
+				searchHelper(node->left, target, distanceTol, depth+1, ids);
+			if((target[depth%2]+distanceTol)>node->point[depth%2])
+				searchHelper(node->right, target, distanceTol, depth+1, ids); 
+
+		}
+
+
+	}
+
+	// return a list of point ids in the tree that are within distance of target
+	std::vector<int> search(std::vector<float> target, float distanceTol)
+	{
+		std::vector<int> ids;
+		searchHelper(root, target, distanceTol, 0, ids);
+		return ids;
+	}
+	
+};
 
 template<typename PointT>
 class ProcessPointClouds {
@@ -38,6 +119,9 @@ public:
 
     std::vector<typename pcl::PointCloud<PointT>::Ptr> Clustering(typename pcl::PointCloud<PointT>::Ptr cloud, float clusterTolerance, int minSize, int maxSize);
 
+
+    void proximityCluster(int indice, typename pcl::PointCloud<PointT>::Ptr cloud, typename pcl::PointCloud<PointT>::Ptr cluster_tmp, float clusterTolerance, KdTree* tree, std::vector<bool>& processed);
+   
     Box BoundingBox(typename pcl::PointCloud<PointT>::Ptr cluster);
 
     void savePcd(typename pcl::PointCloud<PointT>::Ptr cloud, std::string file);
